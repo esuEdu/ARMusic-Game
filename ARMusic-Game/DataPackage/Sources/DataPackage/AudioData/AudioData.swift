ce6ec64f4d401d5fdd92123d53afbe4f618f7101
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-07-24 12:04:28 -0300

    test: create tests for audioData loading
    - Added a new test file `AudioDataTests.swift` in the `Tests` directory.
    - Implemented test cases to verify the loading.
    - `testAudioDataLoading`: Tests if audio data is loaded correctly.

--- ARMusic-Game/DataPackage/Sources/DataPackage/AudioData/AudioData.swift
+++ ARMusic-Game/DataPackage/Sources/DataPackage/AudioData/AudioData.swift
@@ -7,14 +7,27 @@
 
 import Foundation
 
-import Foundation
 
-enum Instruments: String {
+/**
+ Enum representing different musical instruments.
+ 
+ - piano: Represents the piano instrument.
+ */
+public enum Instruments: String {
     case piano
     // Add other instruments as needed
 }
 
-enum Notes: String {
+/**
+ Enum representing musical notes.
+ 
+ - c: Represents the C note.
+ - d: Represents the D note.
+ - e: Represents the E note.
+ - g: Represents the G note.
+ - a: Represents the A note.
+ */
+public enum Notes: String {
     case c = "C"
     case d = "D"
     case e = "E"
@@ -22,19 +35,34 @@ enum Notes: String {
     case a = "A"
 }
 
-struct AudioData {
+/**
+ Struct representing audio data for a specific instrument and note.
+ */
+public struct AudioData {
+    
+    /// The instrument associated with the audio data.
     let instrument: Instruments
+
+    /// The note associated with the audio data.
     let note: Notes
     
+    /// The file name of the audio data based on the instrument and note.
     var fileName: String {
-        return "\(instrument.rawValue)_\(note.rawValue).mpeg"
+        return "\(instrument.rawValue)_\(note.rawValue)"
     }
     
+    /// The URL of the audio file in the main bundle.
     var url: URL? {
-        return Bundle.main.url(forResource: fileName, withExtension: nil)
+        return Bundle.module.url(forResource: fileName, withExtension: "mpeg")
     }
     
+    /**
+     Returns the URL of the audio file.
+     
+     - Returns: An optional URL pointing to the audio file in the main bundle.
+     */
     func getURL() -> URL? {
+        print("Audio file URL: \(String(describing: self.url))")
         return self.url
     }
 }
