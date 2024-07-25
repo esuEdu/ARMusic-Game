0d592f9100c0da34dc3d4b64c55660ed745f4b34
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-07-25 17:43:57 -0300

    Implementar colocação de objetos com gestos e remover código comentado
    Detalhes:
    
    Implementada a função handleTap para colocar objetos na cena AR ao tocar na tela.
    Criado um AnchorEntity no local do toque e adicionado um ModelEntity como caixa azul com materiais metálicos.
    Adicionado o ModelEntity à cena AR e configurados gestos de interação para o modelo.
    Removido o código comentado que estava relacionado à criação de entidades e manipulação de gestos.

--- ARMusic-Game/ARPackage/Sources/ARPackage/Components/MainARView.swift
+++ ARMusic-Game/ARPackage/Sources/ARPackage/Components/MainARView.swift
@@ -34,41 +34,22 @@ class MainARView: ARView {
         if let firstResult = results.first {
             
             
-            let anchor = AnchorEntity(world: firstResult.worldTransform)
+            let anchor = AnchorEntity(raycastResult: firstResult)
             
-            anchor.addChild(box)
+            let modelEntity = ModelEntity(mesh: MeshResource.generateBox(size: 0.3))
+            modelEntity.generateCollisionShapes(recursive: true)
+            modelEntity.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+            
+            
+            
+            anchor.addChild(modelEntity)
             
             arView.scene.addAnchor(anchor)
+            
+            arView.installGestures(.all, for: modelEntity)
         }
     }
     
-    /*
-     
-     @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
-         
-         guard let view = self.view else { return }
-         
-         let tapLocation = recognizer.location(in: view)
-         let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
-         
-         if let result = results.first {
-             
-             // ARAnchor - ARKit Framework
-             // AnchorEntity - RealityKit Framework
-             
-             let anchorEntity = AnchorEntity(raycastResult: result)
-             
-             let modelEntity = ModelEntity(mesh: MeshResource.generateBox(size: 0.3))
-             modelEntity.generateCollisionShapes(recursive: true)
-             modelEntity.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
-             anchorEntity.addChild(modelEntity)
-             view.scene.addAnchor(anchorEntity)
-             
-             view.installGestures(.all, for: modelEntity)
-         }
-     }
-
-     */
     
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
