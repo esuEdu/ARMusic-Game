60cf87a4c1c4ade6b7ab73742ec5d7c21e3b3354
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-07-25 17:39:46 -0300

    Adicionar configuração de ARKit e RealityKit com colocação de objetos e manipulação de gestos
    Adicionada a classe MainARView que estende ARView para gerenciar a configuração do ARKit e RealityKit.
    Configurada a sessão AR com ARWorldTrackingConfiguration, incluindo detecção de planos horizontal e vertical e texturização automática do ambiente.
    Implementado o coaching para melhor orientação do usuário em AR.
    Adicionado reconhecimento de gestos para lidar com toques do usuário e posicionar objetos na cena AR.
    Incluído código de exemplo para criação e interação com entidades de modelo, comentado para referência futura.

--- ARMusic-Game/ARPackage/Sources/ARPackage/Components/MainARView.swift
+++ ARMusic-Game/ARPackage/Sources/ARPackage/Components/MainARView.swift
@@ -42,6 +42,33 @@ class MainARView: ARView {
         }
     }
     
+    /*
+     
+     @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+         
+         guard let view = self.view else { return }
+         
+         let tapLocation = recognizer.location(in: view)
+         let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+         
+         if let result = results.first {
+             
+             // ARAnchor - ARKit Framework
+             // AnchorEntity - RealityKit Framework
+             
+             let anchorEntity = AnchorEntity(raycastResult: result)
+             
+             let modelEntity = ModelEntity(mesh: MeshResource.generateBox(size: 0.3))
+             modelEntity.generateCollisionShapes(recursive: true)
+             modelEntity.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+             anchorEntity.addChild(modelEntity)
+             view.scene.addAnchor(anchorEntity)
+             
+             view.installGestures(.all, for: modelEntity)
+         }
+     }
+
+     */
     
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
