17d8860ad4a20099b41f9bfa973a07ab317408a4
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-08-01 16:58:09 -0300

    Arquivo para a o component 'Custom3DView' criado na pasta çomponents'
    - A struct struct Custom3DView: UIViewRepresentable , foi adicionado lá

--- ARMusic-Game/IpadOS/View/StartView.swift
+++ ARMusic-Game/IpadOS/View/StartView.swift
@@ -103,39 +103,3 @@ struct StartView: View {
     StartView()
 }
 
-struct Custom3DView: UIViewRepresentable {
-    
-    @Binding var scene: SCNScene?
-    var rotation: (x: Double, y: Double)
-    
-    func makeUIView(context: Context) -> some UIView {
-        let view = SCNView()
-        view.allowsCameraControl = false
-        view.autoenablesDefaultLighting = true
-        view.antialiasingMode = .multisampling2X
-        view.backgroundColor = .clear
-        
-        if let scene = scene {
-            view.scene = scene
-        }
-        
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        guard let scene = scene else { return }
-        
-        let scnView = uiView as! SCNView
-        if scnView.scene == nil {
-            scnView.scene = scene
-        }
-        
-        let chairNode = scene.rootNode.childNodes.first
-        chairNode?.eulerAngles.x = Float(rotation.x * .pi / 180)
-        chairNode?.eulerAngles.y = Float(rotation.y * .pi / 180)
-        
-        print("x: \(chairNode?.eulerAngles.x ?? 0)")
-        print("y: \(chairNode?.eulerAngles.y ?? 0)")
-    }
-}
- 
