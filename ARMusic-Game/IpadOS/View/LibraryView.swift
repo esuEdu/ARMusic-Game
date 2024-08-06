d51ae4833aaee4bc3fae527b507595a0cd3a5f27
Thiago Menezes <113785728+Thiago-Develooper@users.noreply.github.com>
2024-08-06 10:00:13 -0300

    refatoração do código

--- ARMusic-Game/IpadOS/View/LibraryView.swift
+++ ARMusic-Game/IpadOS/View/LibraryView.swift
@@ -1,9 +1,18 @@
 import SwiftUI
 
+struct CDItem: Identifiable {
+    let id = UUID()
+    let text: String
+}
+
 struct LibraryView: View {
     
-    private var data: [Int] = Array(1...20)
-    private let colors: [Color] = [.red, .green, .blue, .yellow]
+    private var data: [CDItem] = [
+        CDItem(text: "Item 1"),
+        CDItem(text: "Item 2"),
+        CDItem(text: "Item 3"),
+        CDItem(text: "Item 4"),
+    ]
     private let fixedColumns = [
         GridItem(),
         GridItem()
@@ -11,8 +20,8 @@ struct LibraryView: View {
     
     @State private var dragOffset: CGSize = .zero
     @State private var isDragging = false
-    @State private var draggedItem: Int? = nil
-    @State private var itemPositions: [Int: CGSize] = [:]
+    @State private var draggedItem: CDItem? = nil
+    @State private var itemPositions: [UUID: CGSize] = [:]
     
     @State private var searchText: String = ""
     
@@ -25,13 +34,33 @@ struct LibraryView: View {
                     scroll
                 }
                 .frame(width: UIScreen.main.bounds.width * 0.6712)
+                .foregroundStyle(.gray)
                 
                 //MARK: - Tocador
                 player
                     .frame(width: UIScreen.main.bounds.width * 0.323)
+                    .foregroundStyle(.black.opacity(0.8))
             }
         }
     }
+    
+    func cd() -> some View {
+        VStack {
+            Rectangle()
+                .frame(
+                    width: UIScreen.main.bounds.width * 0.183,
+                    height: UIScreen.main.bounds.height * 0.244
+                )
+                .padding(.bottom, 40)
+            
+            Text("Lorem ipsum dolor sit amet consecteur adpiscing elit, sed do ejusmod tempor.")
+                .bold()
+                .foregroundStyle(.black)
+        }
+    }
+    
+    
+    
     
     func btn(_ text: String) -> some View {
         RoundedRectangle(cornerRadius: 25.0)
@@ -52,10 +81,7 @@ struct LibraryView: View {
     var toolbar: some View {
         HStack {
             Circle()
-                .frame(
-                    width: UIScreen.main.bounds.width * 0.1025,
-                    height: UIScreen.main.bounds.height * 0.1367
-                )
+                .frame(width: UIScreen.main.bounds.width * 0.1025,height: UIScreen.main.bounds.height * 0.1367)
                 .foregroundStyle(.gray)
                 .overlay {
                     Image(systemName: "chevron.left")
@@ -63,13 +89,13 @@ struct LibraryView: View {
                         .fontWeight(.heavy)
                         .foregroundStyle(.black)
                 }
-
+            
             Spacer()
             HStack {
                 Image(systemName: "magnifyingglass")
                     .foregroundColor(.gray)
                     .padding(.leading, 8)
-
+                
                 TextField("Pesquisar", text: $searchText)
                     .padding(8)
                     .background(Color.gray.opacity(0.2))
@@ -77,7 +103,6 @@ struct LibraryView: View {
             }
             .frame(width: UIScreen.main.bounds.width * 0.5)
             Spacer()
-         
             
             Circle()
                 .frame(
@@ -91,7 +116,7 @@ struct LibraryView: View {
                         .fontWeight(.heavy)
                         .foregroundStyle(.black)
                 }
-
+            
         }
     }
     
@@ -106,23 +131,28 @@ struct LibraryView: View {
                         .foregroundStyle(.gray)
                         .padding()
                         .overlay {
-                            Text("Lorem ipsum dolor site amet consectur adpiscing elit, sed do ejusmod tempor.")
+                            Text("Lorem ipsum dolor sit amet consectur adipiscing elit, sed do eiusmod tempor.")
                                 .bold()
                                 .padding()
                         }
-                    Rectangle()
-                        .foregroundStyle(.gray)
-                        .padding()
-                        .overlay {
-                            tocador
-                        }
                     
+                    Rectangle()
+                        .foregroundStyle(.green)
+                        .padding()
+                        .frame(
+                            width: UIScreen.main.bounds.width * 0.2672,
+                            height: UIScreen.main.bounds.height * 0.3564
+                        )
                     Circle()
                         .overlay {
                             Image(systemName: "pause.fill")
                                 .bold()
                                 .foregroundStyle(.red)
                         }
+                        .overlay {
+                            tocador
+                        }
+                    
                     
                     //                    Button("Entrar") {
                     //                        print("oie")
@@ -147,40 +177,40 @@ struct LibraryView: View {
     var scroll: some View {
         ScrollView(.vertical) {
             LazyVGrid(columns: fixedColumns) {
-                ForEach(data, id: \.self) { number in
+                ForEach(data) { item in
                     ZStack {
-                        Rectangle()
-                            .foregroundStyle(colors[number % 4])
-                            .frame(height: 150)
-                            .offset(itemPositions[number] ?? .zero)
+                        cd()
+                            .offset(itemPositions[item.id] ?? .zero)
                             .gesture(
                                 DragGesture()
                                     .onChanged { value in
                                         isDragging = true
-                                        draggedItem = number
+                                        draggedItem = item
                                         dragOffset = value.translation
-                                        itemPositions[number] = dragOffset
+                                        itemPositions[item.id] = dragOffset
                                     }
                                     .onEnded { _ in
                                         isDragging = false
-                                        print("Retângulo \(number) movido para: \(dragOffset)")
+                                        print("Item \(item.text) movido para: \(dragOffset)")
                                         draggedItem = nil
                                         dragOffset = .zero
                                     }
                             )
                         
                     }
-                    .zIndex(draggedItem == number ? 1 : 0)
-                    .overlay {
-                        Text("\(number)")
-                            .font(.largeTitle)
-                            .bold()
-                            .fontDesign(.rounded)
-                    }
+                    //       .zIndex(draggedItem == item ? 1 : 0)
+                    //                    .overlay {
+                    //                        Text(item.text)
+                    //                            .font(.largeTitle)
+                    //                            .bold()
+                    //                            .fontDesign(.rounded)
+                    //                            .foregroundStyle(.black)
+                    //                    }
                 }
             }
         }
         .frame(width: UIScreen.main.bounds.width * 0.66)
+        .background(Color.blue)
     }
     
     var tocador: some View {
@@ -190,22 +220,6 @@ struct LibraryView: View {
             .overlay(
                 Text("Tocador")
                     .foregroundColor(.white)
-            )
-    }
-    
-    func cdCapa() -> some View {
-        Rectangle()
-            .frame(
-                width: UIScreen.main.bounds.width * 0.183,
-                height: UIScreen.main.bounds.height * 0.244
-            )
-    }
-    
-    func cd() -> some View {
-        Circle()
-            .frame(
-                width: UIScreen.main.bounds.width * 0.183,
-                height: UIScreen.main.bounds.height * 0.244
             )
     }
 }
